8eb95da2cb53e17d42713b603b0b333906d3c5a0
HAN011 <3294277501@qq.com>
2024-09-04 14:31:55 +0800

    Site updated: 2024-09-04 14:31:54

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/03/hello-world/"];function toRandomPost(){
+var posts=["2024/09/04/initial/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
