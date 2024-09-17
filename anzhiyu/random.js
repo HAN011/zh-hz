49809fb420544f1b424e5305b5566f36e5f0c866
HAN011 <3294277501@qq.com>
2024-09-17 22:25:57 +0800

    Site updated: 2024-09-17 22:25:56

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/09/04/initial/","2024/09/04/轮腿开源/"];function toRandomPost(){
+var posts=["2024/09/17/代码学习/","2024/09/04/initial/","2024/09/04/轮腿开源/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
